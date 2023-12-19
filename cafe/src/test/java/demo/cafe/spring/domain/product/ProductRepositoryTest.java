d4a66acb67f916e9b5a70f0a827f933fe3b47203
zeze <thwjd2717@gmail.com>
2023-12-19 16:58:05 +0900

    test: repository 단위 테스트

---
+++ cafe/src/test/java/demo/cafe/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,66 @@
+package demo.cafe.spring.domain.product;
+
+import static demo.cafe.spring.domain.product.ProductSellingStatus.*;
+import static demo.cafe.spring.domain.product.ProductType.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.*;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다")
+    @Test
+    void findBySellingStatusIn() {
+        // given
+        Product product1 = Product.builder()
+            .productNumber("001")
+            .type(HANDMADE)
+            .sellingStatus(SELLING)
+            .name("아메리카노")
+            .price(4000)
+            .build();
+
+        Product product2 = Product.builder()
+            .productNumber("002")
+            .type(HANDMADE)
+            .sellingStatus(HOLD)
+            .name("카페라떼")
+            .price(4500)
+            .build();
+
+        Product product3 = Product.builder()
+            .productNumber("003")
+            .type(HANDMADE)
+            .sellingStatus(STOP_SELLING)
+            .name("딸기우유")
+            .price(6000)
+            .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        // SELLING, HOLD 상태만 조회하고 싶을 때
+        List<Product> products = productRepository.findBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        assertThat(products).hasSize(2)
+            .extracting("productNumber", "name", "sellingStatus")
+            .containsExactlyInAnyOrder( // 순서 상관 없음
+                tuple("001", "아메리카노", SELLING),
+                tuple("002", "카페라떼", HOLD)
+            );
+    }
+}
